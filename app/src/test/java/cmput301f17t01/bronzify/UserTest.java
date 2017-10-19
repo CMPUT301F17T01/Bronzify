535df825390ec0736a6350edf1f4dd33af75b13d
Kevin de Haan <kbehaan@gmail.com>
2017-10-19 17:59:02 -0600

    Unit test init definition

---
+++ app/src/test/java/cmput301f17t01/bronzify/UserTest.java
@@ -0,0 +1,14 @@
+package cmput301f17t01.bronzify;
+
+import org.junit.Test;
+
+/**
+ * Created by kdehaan on 19/10/17.
+ */
+
+public class UserTest {
+    @Test
+    public void test() {
+
+    }
+}
