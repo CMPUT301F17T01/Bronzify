1c6540da79613df19a0f111825ddb8bb3c9924d7
Kevin de Haan <kbehaan@gmail.com>
2017-10-19 17:59:02 -0600

    Unit test init definition

---
+++ app/src/test/java/cmput301f17t01/bronzify/HabitEventTest.java
@@ -0,0 +1,14 @@
+package cmput301f17t01.bronzify;
+
+import org.junit.Test;
+
+/**
+ * Created by kdehaan on 19/10/17.
+ */
+
+public class HabitEventTest {
+    @Test
+    public void test() {
+
+    }
+}
---
+++ app/src/test/java/cmput301f17t01/bronzify/HabitTypeTest.java
@@ -0,0 +1,14 @@
+package cmput301f17t01.bronzify;
+
+import org.junit.Test;
+
+/**
+ * Created by kdehaan on 19/10/17.
+ */
+
+public class HabitTypeTest {
+    @Test
+    public void test() {
+
+    }
+}
