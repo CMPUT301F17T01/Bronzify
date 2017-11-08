d85d21608df5c58f1f282db577a3a55f576bdf35
Jacob <jblazusi@ualberta.ca>
2017-11-07 19:43:54 -0700

    crash on launch

--- app/src/main/java/cmput301f17t01/bronzify/activities/MyHabitsActivity.java
+++ app/src/main/java/cmput301f17t01/bronzify/activities/MyHabitsActivity.java
@@ -4,6 +4,14 @@ import android.app.Fragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Button;
 
 import java.time.DayOfWeek;
@@ -16,7 +24,7 @@ import cmput301f17t01.bronzify.R;
 /**
  * Created by owenm_000 on 11/1/2017.
  */
-public class MyHabitsActivity extends Activity {
+public class MyHabitsActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private Intent intent;
     private ArrayList<HabitType> eventList;
@@ -72,5 +80,62 @@ public class MyHabitsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_habits);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.MyHome) {
+            Intent searchIntent = new Intent(MyHabitsActivity.this, MyHomeActivity.class);
+            startActivity(searchIntent);
+            overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+        }
+//        } else if (id == R.id.fileimport) {
+//            Intent searchIntent = new Intent(MainActivity.this, fileimport.class);
+//            startActivity(searchIntent);
+//            overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+//        } else if (id == R.id.slideshow) {
+//            Intent searchIntent = new Intent(MainActivity.this, fileimport.class);
+//            startActivity(searchIntent);
+//            overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+//        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
--- app/src/main/java/cmput301f17t01/bronzify/activities/MyHomeActivity.java
+++ app/src/main/java/cmput301f17t01/bronzify/activities/MyHomeActivity.java
@@ -1,18 +1,83 @@
 package cmput301f17t01.bronzify.activities;
 
-import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.support.v7.widget.Toolbar;
 
 import cmput301f17t01.bronzify.R;
 
 /**
  * Created by owenm_000 on 11/1/2017.
  */
-public class MyHomeActivity extends Activity {
+public class MyHomeActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_home);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+    }
+
+    @Override
+    public void onBackPressed(){
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)){
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.nav_drawer, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item){
+        int id = item.getItemId();
+        if (id == R.id.action_settings){
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == R.id.MyHabits) {
+            Intent searchIntent = new Intent(MyHomeActivity.this, MyHabitsActivity.class);
+            startActivity(searchIntent);
+            overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+        }
+//        } else if (id == R.id.fileimport) {
+//            Intent searchIntent = new Intent(MainActivity.this, fileimport.class);
+//            startActivity(searchIntent);
+//            overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+//        } else if (id == R.id.slideshow) {
+//            Intent searchIntent = new Intent(MainActivity.this, fileimport.class);
+//            startActivity(searchIntent);
+//            overridePendingTransition(R.anim.pull_in_right, R.anim.push_out_left);
+//        }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
