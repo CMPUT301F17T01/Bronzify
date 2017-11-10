e2a64dc689943c1cb5b2abd443cc88fa518a9f4d
Kevin de Haan <kbehaan@gmail.com>
2017-11-10 16:34:47 -0700

    experimental jest/elasticsearch model implementation

--- app/src/main/java/cmput301f17t01/bronzify/models/ElasticSearch.java
+++ app/src/main/java/cmput301f17t01/bronzify/models/ElasticSearch.java
@@ -1,6 +1,15 @@
 package cmput301f17t01.bronzify.models;
 
+
+import android.os.AsyncTask;
+import android.util.Log;
+
+import com.searchly.jestdroid.DroidClientConfig;
+import com.searchly.jestdroid.JestClientFactory;
 import com.searchly.jestdroid.JestDroidClient;
+
+import io.searchbox.core.DocumentResult;
+import io.searchbox.core.Index;
 
 /**
  * Created by kdehaan on 10/11/17.
@@ -8,5 +17,41 @@ import com.searchly.jestdroid.JestDroidClient;
 
 public class ElasticSearch {
     private static JestDroidClient client;
+
+    public static class PostUser extends AsyncTask<User, Void, Void> {
+        @Override
+        protected Void doInBackground(User... users) {
+            verifySettings();
+            for (User user : users) {
+                Index index = new Index.Builder(user)
+                        .index("Testing")
+                        .type("user")
+                        .build();
+                try {
+                    DocumentResult result = client.execute(index);
+                    if (result.isSucceeded()) {
+                        user.setUserID(result.getId());
+                    } else {
+                        Log.i("Error", "Elasticsearch was not able to add the user");
+                    }
+                } catch (Exception e) {
+                    Log.i("Error", "The application failed to build and send the user");
+                }
+            }
+            return null;
+        }
+    }
+
+    public static void verifySettings() {
+        if (client == null) {
+            DroidClientConfig.Builder builder = new DroidClientConfig
+                    .Builder("http://cmput301.softwareprocess.es:8080/cmput301f17t01_bronzify");
+            DroidClientConfig config = builder.build();
+
+            JestClientFactory factory = new JestClientFactory();
+            factory.setDroidClientConfig(config);
+            client = (JestDroidClient) factory.getObject();
+        }
+    }
 
 }
