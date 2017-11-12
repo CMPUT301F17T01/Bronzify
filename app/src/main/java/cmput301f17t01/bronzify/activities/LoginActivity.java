1435b3e8c9de3e9b0cd212e443528d4bda89f913
Kevin de Haan <kbehaan@gmail.com>
2017-11-12 00:28:09 -0700

    minor hotfixes (empty input)

--- app/src/main/java/cmput301f17t01/bronzify/activities/LoginActivity.java
+++ app/src/main/java/cmput301f17t01/bronzify/activities/LoginActivity.java
@@ -1,27 +1,23 @@
 package cmput301f17t01.bronzify.activities;
 
-import android.content.DialogInterface;
+
 import android.content.Intent;
-import android.os.Handler;
-import android.support.v7.app.AlertDialog;
+
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
-import android.view.animation.AlphaAnimation;
-import android.view.animation.Animation;
+
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
 import cmput301f17t01.bronzify.R;
 import cmput301f17t01.bronzify.controllers.LoginController;
-import cmput301f17t01.bronzify.models.Locale;
 import cmput301f17t01.bronzify.models.User;
 
 public class LoginActivity extends AppCompatActivity {
     private LoginController controller = new LoginController();
-    private Locale locale = Locale.getInstance();
 
 
     @Override
@@ -39,6 +35,10 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 String input = enterId.getText().toString();
+                if (input == "") {
+                    loginInfo.setVisibility(View.VISIBLE);
+                    loginInfo.setText("Please enter a user ID");
+                }
                 User result = controller.checkLogin(input);
                 if (result != null) {
                     Log.i("User", "Found");
@@ -60,6 +60,11 @@ public class LoginActivity extends AppCompatActivity {
             public void onClick(View view) {
 
                 String input = enterId.getText().toString();
+                if (input == "") {
+                    loginInfo.setVisibility(View.VISIBLE);
+                    loginInfo.setText("Please enter a user ID");
+                    return;
+                }
                 User result = controller.checkLogin(input);
                 if (result == null) { //opposite of login
                     Log.i("User", "Not Found");
