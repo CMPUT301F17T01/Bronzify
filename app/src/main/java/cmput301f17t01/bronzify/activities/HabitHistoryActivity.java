d759ea62715daa82ef222342c855bc20aa91061b
noahkryz <nkryzano@ualberta.ca>
2017-11-13 01:36:45 -0700

    Tabs working on history

--- app/src/main/java/cmput301f17t01/bronzify/activities/HabitHistoryActivity.java
+++ app/src/main/java/cmput301f17t01/bronzify/activities/HabitHistoryActivity.java
@@ -4,7 +4,10 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.app.Activity;
 import android.support.design.widget.NavigationView;
+import android.support.design.widget.TabLayout;
+import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
+import android.support.v4.view.ViewPager;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
@@ -18,6 +21,7 @@ import android.widget.ToggleButton;
 
 import java.util.ArrayList;
 
+import cmput301f17t01.bronzify.fragments.ListFragment;
 import cmput301f17t01.bronzify.models.HabitEvent;
 import cmput301f17t01.bronzify.R;
 import cmput301f17t01.bronzify.controllers.NavigationController;
@@ -34,54 +38,42 @@ public class HabitHistoryActivity extends AppCompatActivity implements Navigatio
     private EditText searchBar;
     private Button sideBar;
 
+    private ViewPager viewPager;
+    private TabLayout tabLayout;
+    private DrawerLayout drawer;
+    private String[] pageTitle = {"Feed", "Map"};
+
     //protected void onClickFeed() {}
 
     //protected void onClickMap() {}
-
-    // getters and setters
-    public ToggleButton getToggleFilter() {
-        return toggleFilter;
-    }
-
-    public void setToggleFilter(ToggleButton toggleFilter) {
-        this.toggleFilter = toggleFilter;
-    }
-
-    public ArrayList<HabitEvent> getHabitEvents() {
-        return habitEvents;
-    }
-
-    public void setHabitEvents(ArrayList<HabitEvent> habitEvents) {
-        this.habitEvents = habitEvents;
-    }
-
-    public EditText getSearchBar() {
-        return searchBar;
-    }
-
-    public void setSearchBar(EditText searchBar) {
-        this.searchBar = searchBar;
-    }
-
-    public Button getSideBar() {
-        return sideBar;
-    }
-
-    public void setSideBar(Button sideBar) {
-        this.sideBar = sideBar;
-    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_habit_history);
 
-        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        if (savedInstanceState == null) {
+            FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+            ListFragment fragment = new ListFragment();
+            transaction.replace(R.id.sample_content_fragment, fragment);
+            transaction.commit();
+        }
+
+        viewPager = findViewById(R.id.view_pager);
+        Toolbar toolbar = findViewById(R.id.toolbar);
+        drawer = findViewById(R.id.drawer_layout);
+
         setSupportActionBar(toolbar);
-        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        DrawerLayout drawer = findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
-        drawer.setDrawerListener(toggle);
+        drawer.addDrawerListener(toggle);
         toggle.syncState();
+
+        //Create 2 tabs
+        tabLayout = findViewById(R.id.tab_layout);
+        for (int i = 0; i < 2; i++) {
+            tabLayout.addTab(tabLayout.newTab().setText(pageTitle[i]));
+        }
 
         NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
         navigationView.setNavigationItemSelectedListener(this);
@@ -125,4 +117,38 @@ public class HabitHistoryActivity extends AppCompatActivity implements Navigatio
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    // getters and setters
+    public ToggleButton getToggleFilter() {
+        return toggleFilter;
+    }
+
+    public void setToggleFilter(ToggleButton toggleFilter) {
+        this.toggleFilter = toggleFilter;
+    }
+
+    public ArrayList<HabitEvent> getHabitEvents() {
+        return habitEvents;
+    }
+
+    public void setHabitEvents(ArrayList<HabitEvent> habitEvents) {
+        this.habitEvents = habitEvents;
+    }
+
+    public EditText getSearchBar() {
+        return searchBar;
+    }
+
+    public void setSearchBar(EditText searchBar) {
+        this.searchBar = searchBar;
+    }
+
+    public Button getSideBar() {
+        return sideBar;
+    }
+
+    public void setSideBar(Button sideBar) {
+        this.sideBar = sideBar;
+    }
+
 }
