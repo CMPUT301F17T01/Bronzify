a58eac43e63660b17a5ab189eccd545021573925
Jacob <jblazusi@ualberta.ca>
2017-11-29 16:23:08 -0700

    launching camera

--- app/src/main/java/cmput301f17t01/bronzify/fragments/PictureFragment.java
+++ app/src/main/java/cmput301f17t01/bronzify/fragments/PictureFragment.java
@@ -1,20 +1,94 @@
 package cmput301f17t01.bronzify.fragments;
 
+import android.app.Activity;
+import android.support.v4.app.Fragment;
+import android.content.ContentResolver;
+import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.media.Image;
+import android.net.Uri;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.annotation.Nullable;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.Toast;
+
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+import cmput301f17t01.bronzify.R;
+
+import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by jblazusi on 2017-11-01.
  */
 
-public class PictureFragment {
+public class PictureFragment extends Fragment {
 
-    private Button addPicture;
-    private Button deletePicture;
-    private Image picture;
+    private static final int REQUEST_IMAGE_CAPTURE = 1;
 
-    private void displayPicture(){
-        return;
+    Button button;
+    ImageView imageView;
+
+    public PictureFragment() {
+        //CONSTRUCTOR
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+
+        final View rootView = inflater.inflate(R.layout.habit_event_tab_picture,
+                container, false);
+
+        button = (Button) rootView.findViewById(R.id.buttonpic);
+        imageView = (ImageView) rootView.findViewById(R.id.testing_pic);
+
+        button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+                startActivityForResult(intent,
+                        REQUEST_IMAGE_CAPTURE);
+
+            }
+        });
+
+        return rootView;
+
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        if (requestCode == REQUEST_IMAGE_CAPTURE) {
+            if (resultCode == Activity.RESULT_OK) {
+
+                Bitmap bmp = (Bitmap) data.getExtras().get("data");
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+
+                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                byte[] byteArray = stream.toByteArray();
+
+                // convert byte array to Bitmap
+
+                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
+                        byteArray.length);
+
+                imageView.setImageBitmap(bitmap);
+
+            }
+        }
     }
 }
+
 
