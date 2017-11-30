6673d6792fde26da87efe1962a46a5d94bda5aa9
Jacob <jblazusi@ualberta.ca>
2017-11-30 00:25:57 -0700

    needs work with compression

--- app/src/main/java/cmput301f17t01/bronzify/fragments/PictureFragment.java
+++ app/src/main/java/cmput301f17t01/bronzify/fragments/PictureFragment.java
@@ -18,11 +18,14 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
+import android.widget.TextView;
 import android.widget.Toast;
 
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 
 import cmput301f17t01.bronzify.R;
 
@@ -38,6 +41,7 @@ public class PictureFragment extends Fragment {
 
     Button button;
     ImageView imageView;
+    TextView textView;
 
     public PictureFragment() {
         //CONSTRUCTOR
@@ -53,13 +57,14 @@ public class PictureFragment extends Fragment {
         button = (Button) rootView.findViewById(R.id.buttonpic);
         imageView = (ImageView) rootView.findViewById(R.id.testing_pic);
 
+        textView = rootView.findViewById(R.id.bytecount);
+
         button.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
                 Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-                startActivityForResult(intent,
-                        REQUEST_IMAGE_CAPTURE);
+                startActivityForResult(intent, REQUEST_IMAGE_CAPTURE);
 
             }
         });
@@ -76,19 +81,71 @@ public class PictureFragment extends Fragment {
                 Bitmap bmp = (Bitmap) data.getExtras().get("data");
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
-                bmp.compress(Bitmap.CompressFormat.PNG, 100, stream);
+                bmp.compress(Bitmap.CompressFormat.JPEG, 50, stream);   //contradiction with compress.....
                 byte[] byteArray = stream.toByteArray();
 
                 // convert byte array to Bitmap
 
-                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0,
-                        byteArray.length);
+                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
 
                 imageView.setImageBitmap(bitmap);
+
+
+                Bitmap newImage = getResizedBitmap(bmp, 150 );
+
+
+                textView.setText(String.valueOf(newImage.getByteCount()));
 
             }
         }
     }
+
+    /**
+     * reduces the size of the image
+     * @param image
+     * @param maxSize
+     * @return
+     */
+    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
+        int width = image.getWidth();
+        int height = image.getHeight();
+
+        float bitmapRatio = (float)width / (float) height;
+        if (bitmapRatio > 1) {
+            width = maxSize;
+            height = (int) (width / bitmapRatio);
+        } else {
+            height = maxSize;
+            width = (int) (height * bitmapRatio);
+        }
+        return Bitmap.createScaledBitmap(image, width, height, true);
+    }
+
+    // Decodes image and scales it to reduce memory consumption
+    private Bitmap decodeFile(File f) {
+        try {
+            // Decode image size
+            BitmapFactory.Options o = new BitmapFactory.Options();
+            o.inJustDecodeBounds = true;
+            BitmapFactory.decodeStream(new FileInputStream(f), null, o);
+
+            // The new size we want to scale to
+            final int REQUIRED_SIZE=64;
+
+            // Find the correct scale value. It should be the power of 2.
+            int scale = 1;
+            while(o.outWidth / scale / 2 >= REQUIRED_SIZE && o.outHeight / scale / 2 >= REQUIRED_SIZE) {
+                scale *= 2;
+            }
+
+            // Decode with inSampleSize
+            BitmapFactory.Options o2 = new BitmapFactory.Options();
+            o2.inSampleSize = scale;
+            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
+        } catch (FileNotFoundException e) {}
+        return null;
+    }
+
 }
 
 
