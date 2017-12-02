ac9f7a39f2a2b49c29c9bb2637e4370813f9cc4f
Jacob <jblazusi@ualberta.ca>
2017-12-01 23:46:29 -0700

    picture compression

--- app/src/main/java/cmput301f17t01/bronzify/fragments/PictureFragment.java
+++ app/src/main/java/cmput301f17t01/bronzify/fragments/PictureFragment.java
@@ -2,34 +2,20 @@ package cmput301f17t01.bronzify.fragments;
 
 import android.app.Activity;
 import android.support.v4.app.Fragment;
-import android.content.ContentResolver;
 import android.content.Intent;
 import android.graphics.Bitmap;
-import android.graphics.BitmapFactory;
-import android.media.Image;
-import android.net.Uri;
 import android.os.Bundle;
-import android.os.Environment;
 import android.provider.MediaStore;
-import android.support.annotation.Nullable;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
-
-
 import java.io.ByteArrayOutputStream;
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.FileNotFoundException;
-
 import cmput301f17t01.bronzify.R;
+import cmput301f17t01.bronzify.adapters.ImageAdapter;
 
-import static android.app.Activity.RESULT_OK;
 
 /**
  * Created by jblazusi on 2017-11-01.
@@ -38,10 +24,12 @@ import static android.app.Activity.RESULT_OK;
 public class PictureFragment extends Fragment {
 
     private static final int REQUEST_IMAGE_CAPTURE = 1;
+    private static final int PIXELS = 100;
 
     Button button;
     ImageView imageView;
-    TextView textView;
+
+    ImageView circularImageView;
 
     public PictureFragment() {
         //CONSTRUCTOR
@@ -56,8 +44,7 @@ public class PictureFragment extends Fragment {
 
         button = (Button) rootView.findViewById(R.id.buttonpic);
         imageView = (ImageView) rootView.findViewById(R.id.testing_pic);
-
-        textView = rootView.findViewById(R.id.bytecount);
+        circularImageView = (ImageView) rootView.findViewById(R.id.circleView);
 
         button.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -79,73 +66,21 @@ public class PictureFragment extends Fragment {
             if (resultCode == Activity.RESULT_OK) {
 
                 Bitmap bmp = (Bitmap) data.getExtras().get("data");
+
+                Log.d("PHOTO","Size in KB before compression: " + bmp.getByteCount() / 1000);
+
                 ByteArrayOutputStream stream = new ByteArrayOutputStream();
 
-                bmp.compress(Bitmap.CompressFormat.JPEG, 50, stream);   //contradiction with compress.....
+                bmp.compress(Bitmap.CompressFormat.JPEG, 100, stream);
                 byte[] byteArray = stream.toByteArray();
 
-                // convert byte array to Bitmap
+                Log.d("PHOTO","Size in KB after compression: " + byteArray.length / 1000);
 
-                Bitmap bitmap = BitmapFactory.decodeByteArray(byteArray, 0, byteArray.length);
-
-                imageView.setImageBitmap(bitmap);
-
-
-                Bitmap newImage = getResizedBitmap(bmp, 150 );
-
-
-                textView.setText(String.valueOf(newImage.getByteCount()));
-
+                Bitmap circularBitmap = ImageAdapter.getRoundedCornerBitmap(bmp, PIXELS);
+                circularImageView.setImageBitmap(circularBitmap);
             }
         }
     }
-
-    /**
-     * reduces the size of the image
-     * @param image
-     * @param maxSize
-     * @return
-     */
-    public Bitmap getResizedBitmap(Bitmap image, int maxSize) {
-        int width = image.getWidth();
-        int height = image.getHeight();
-
-        float bitmapRatio = (float)width / (float) height;
-        if (bitmapRatio > 1) {
-            width = maxSize;
-            height = (int) (width / bitmapRatio);
-        } else {
-            height = maxSize;
-            width = (int) (height * bitmapRatio);
-        }
-        return Bitmap.createScaledBitmap(image, width, height, true);
-    }
-
-    // Decodes image and scales it to reduce memory consumption
-    private Bitmap decodeFile(File f) {
-        try {
-            // Decode image size
-            BitmapFactory.Options o = new BitmapFactory.Options();
-            o.inJustDecodeBounds = true;
-            BitmapFactory.decodeStream(new FileInputStream(f), null, o);
-
-            // The new size we want to scale to
-            final int REQUIRED_SIZE=64;
-
-            // Find the correct scale value. It should be the power of 2.
-            int scale = 1;
-            while(o.outWidth / scale / 2 >= REQUIRED_SIZE && o.outHeight / scale / 2 >= REQUIRED_SIZE) {
-                scale *= 2;
-            }
-
-            // Decode with inSampleSize
-            BitmapFactory.Options o2 = new BitmapFactory.Options();
-            o2.inSampleSize = scale;
-            return BitmapFactory.decodeStream(new FileInputStream(f), null, o2);
-        } catch (FileNotFoundException e) {}
-        return null;
-    }
-
 }
 
 
