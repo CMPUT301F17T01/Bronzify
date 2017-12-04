abfd5281e4229e42a825784b0b73b804d6b3c6ca
Noah <nkryzano@ualberta.ca>
2017-12-04 05:03:59 -0700

    6 test cases completed

--- app/src/androidTest/java/cmput301f17t01/bronzify/EspressoTesting.java
+++ app/src/androidTest/java/cmput301f17t01/bronzify/EspressoTesting.java
@@ -21,6 +21,9 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.MethodSorters;
 
+import java.util.Calendar;
+import java.util.Date;
+
 import cmput301f17t01.bronzify.activities.LoginActivity;
 import cmput301f17t01.bronzify.activities.MyHomeActivity;
 
@@ -56,13 +59,44 @@ public class EspressoTesting {
     public ActivityTestRule<LoginActivity> mActivityRule =
             new ActivityTestRule(LoginActivity.class);
 
+    /**
+     * This test case will create the users with the name "test_user_001", "test_user_001"
+     * and "follow_user_001". It is commented out, because the users are already created.
+     * It can easily be run to create the users in a single step.
+     *
+     */
+    /*@Test
+    public void test0CreateUsers() {
+        //Create user "test_user_001"
+        onView(withId(R.id.enter_id)).perform(typeText("test_user_001"));
+        onView(withId(R.id.enter_id)).check(matches(withText("test_user_001")));
+        onView(withId(R.id.login_button)).perform(closeSoftKeyboard());
+        onView(withId(R.id.register_button)).perform(click());
+
+        //Create user "follow_user_001"
+        onView(withId(R.id.enter_id)).perform(typeText("follow_user_001"));
+        onView(withId(R.id.enter_id)).check(matches(withText("follow_user_001")));
+        onView(withId(R.id.login_button)).perform(closeSoftKeyboard());
+        onView(withId(R.id.register_button)).perform(click());
+
+        //Create user "follow_user_002"
+        onView(withId(R.id.enter_id)).perform(typeText("follow_user_002"));
+        onView(withId(R.id.enter_id)).check(matches(withText("follow_user_002")));
+        onView(withId(R.id.login_button)).perform(closeSoftKeyboard());
+        onView(withId(R.id.register_button)).perform(click());
+    }*/
+
     @Test
-    public void test1CreateNewHabitEvent() {
+    /**
+     * This test case will create a new habit type, with the name Running. It will
+     * log in and log out, and ensure that the data entered is correct
+     *
+     */
+    public void test1CreateNewHabitType() {
         //Login
         onView(withId(R.id.enter_id)).perform(typeText("test_user_001"));
         onView(withId(R.id.enter_id)).check(matches(withText("test_user_001")));
         onView(withId(R.id.login_button)).perform(closeSoftKeyboard());
-
         onView(withId(R.id.login_button)).perform(click());
 
         onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
@@ -92,6 +126,12 @@ public class EspressoTesting {
         onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.LogOut));
     }
 
+    /**
+     * This test case will edit the habit type that was created in test1. It will change the name
+     * of the habit from "Running" to "Skating", as well as removing Wednesday from the recurring
+     * days every week.
+     *
+     */
     @Test
     public void test2EditHabitType() {
         //Login
@@ -125,6 +165,11 @@ public class EspressoTesting {
     }
 
     @Test
+    /**
+     * This test case will delete the habit type that was created in test2. The habit event skating
+     * will no longer exist after it has been run.
+     *
+     */
     public void test3DeleteHabitType() {
         //Login
         onView(withId(R.id.enter_id)).perform(typeText("test_user_001"));
@@ -149,13 +194,94 @@ public class EspressoTesting {
     }
 
     @Test
-    public void test4followUser() {
+    /**
+     * This test case will log in as "follow_user_001" and will follow the user "test_user_001".
+     * Then "test_user_001" will log into their account to accept the follow request.
+     *
+     */
+    public void test4FollowUser() {
         //Login
         onView(withId(R.id.enter_id)).perform(typeText("follow_user_001"));
         onView(withId(R.id.enter_id)).check(matches(withText("follow_user_001")));
         onView(withId(R.id.login_button)).perform(closeSoftKeyboard());
         onView(withId(R.id.login_button)).perform(click());
 
-        //Set
+        //
+    }
+
+    @Test
+    /**
+     * This test case will add two new events to the user "test_user_002" and demonstrate that
+     * all the events that will occur today will display on the MyHome tab. The first event will
+     * not be completed and the second event will be completed.
+     *
+     */
+    public void test5Today() {
+        //Login
+        onView(withId(R.id.enter_id)).perform(typeText("test_user_002"));
+        onView(withId(R.id.enter_id)).check(matches(withText("test_user_002")));
+        onView(withId(R.id.login_button)).perform(closeSoftKeyboard());
+        onView(withId(R.id.login_button)).perform(click());
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.MyHabits));
+
+        //Create the first habit
+        onView(withId(R.id.createNewHabit)).perform(click());
+        onView(withId(R.id.textHabitName)).perform(typeText("Rowing"));
+        onView(withId(R.id.textHabitName)).check(matches(withText("Rowing")));
+        onView(withId(R.id.textHabitReason)).perform(typeText("Get fit"));
+        onView(withId(R.id.textHabitReason)).check(matches(withText("Get fit")));
+        onView(withId(R.id.buttonSelectDate)).perform(click());
+        Calendar cal = Calendar.getInstance();
+        int day = cal.get(Calendar.DAY_OF_MONTH);
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2017, 12, day));
+        onView(withText("OK")).perform(click());
+        onView(withId(R.id.buttonMonday)).perform(click());
+        onView(withId(R.id.buttonCreate)).perform(closeSoftKeyboard());
+        onView(withId(R.id.buttonCreate)).perform(click());
+
+        //Create the second habit
+        onView(withId(R.id.createNewHabit)).perform(click());
+        onView(withId(R.id.textHabitName)).perform(typeText("Swimming"));
+        onView(withId(R.id.textHabitName)).check(matches(withText("Swimming")));
+        onView(withId(R.id.textHabitReason)).perform(typeText("Too cold outside"));
+        onView(withId(R.id.textHabitReason)).check(matches(withText("Too cold outside")));
+        onView(withId(R.id.buttonSelectDate)).perform(click());
+        onView(withClassName(Matchers.equalTo(DatePicker.class.getName()))).perform(PickerActions.setDate(2017, 12, day));
+        onView(withText("OK")).perform(click());
+        onView(withId(R.id.buttonMonday)).perform(click());
+        onView(withId(R.id.buttonCreate)).perform(closeSoftKeyboard());
+        onView(withId(R.id.buttonCreate)).perform(click());
+
+        //Change to MyHome
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.MyHome));
+
+        //Click on the correct habit in the list
+        onView(withId(R.id.habitTypeRow)).check(matches(withText(containsString("Rowing"))));
+        onView(withId(R.id.habitTypeRow)).perform(click());
+
+        //Click on EVENT FAILED for Rowing
+        //Click on picture and potentially take a picture?
+        //Click on EVENT COMPLETED for Swimming
+        //
+    }
+
+    @Test
+    /**
+     * This test case will show all of the past events that the user has had, completed or not.
+     *
+     */
+    public void test6HabitHistory() {
+        //Login
+        onView(withId(R.id.enter_id)).perform(typeText("test_user_001"));
+        onView(withId(R.id.enter_id)).check(matches(withText("test_user_001")));
+        onView(withId(R.id.login_button)).perform(closeSoftKeyboard());
+        onView(withId(R.id.login_button)).perform(click());
+
+        onView(withId(R.id.drawer_layout)).perform(DrawerActions.open());
+        onView(withId(R.id.nav_view)).perform(NavigationViewActions.navigateTo(R.id.MyHabits));
+
     }
 }
