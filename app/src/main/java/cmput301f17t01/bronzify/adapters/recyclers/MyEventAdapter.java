5ddd095679bd14b679a59326175aef68507b7ed4
Ken Li <krli@ualberta.ca>
2017-12-04 14:42:14 -0700

    Changed Typy/Event Rows (Undo if Broken)

--- app/src/main/java/cmput301f17t01/bronzify/adapters/recyclers/MyEventAdapter.java
+++ app/src/main/java/cmput301f17t01/bronzify/adapters/recyclers/MyEventAdapter.java
@@ -2,6 +2,7 @@ package cmput301f17t01.bronzify.adapters.recyclers;
 
 import android.content.Context;
 import android.content.Intent;
+import android.support.constraint.ConstraintLayout;
 import android.support.v7.widget.RecyclerView;
 import android.util.Log;
 import android.view.LayoutInflater;
@@ -44,31 +45,23 @@ public class MyEventAdapter extends RecyclerView.Adapter<MyEventAdapter.ViewHold
     public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
         private TextView habitEventName;
         private TextView habitEventDate;
-        private RelativeLayout relativeLayout;
+        private ConstraintLayout constraintLayout;
 
 
-        private ViewHolder(View v, Context innerContext) {
+        private ViewHolder(View v) {
             super(v);
             v.setOnClickListener(this);
             habitEventName = v.findViewById(R.id.habitEventRow);
             habitEventDate = v.findViewById(R.id.eventDate);
-            relativeLayout = v.findViewById(R.id.event_row);
+            constraintLayout = v.findViewById(R.id.event_row);
         }
 
         @Override
         public void onClick(View v) {
             int pos = this.getAdapterPosition();
-            HabitEvent habitEvent = entries.get(pos);
-
-            Intent intent = new Intent(mContext, HabitEventActivity.class);
-            
-            //THIS IS THE NEW VERSION
             AppLocale.getInstance().setEvent(entries.get(pos));
 
-            //OLD VERSION BELOW:
-            //intent.putExtra("HABIT_TYPE", habitEvent.getHabitType());
-            //intent.putExtra("GOAL_DATE", habitEvent.getGoalDate().getTime());
-
+            Intent intent = new Intent(mContext, HabitEventActivity.class);
             mContext.startActivity(intent);
         }
     }
@@ -76,7 +69,7 @@ public class MyEventAdapter extends RecyclerView.Adapter<MyEventAdapter.ViewHold
     @Override
     public MyEventAdapter.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
         View v = LayoutInflater.from(mContext).inflate(R.layout.event_row, parent, false);
-        return (new ViewHolder(v, mContext));
+        return (new ViewHolder(v));
     }
 
     @Override
@@ -86,14 +79,14 @@ public class MyEventAdapter extends RecyclerView.Adapter<MyEventAdapter.ViewHold
         holder.habitEventDate.setText(habitEvent.goalDateToString());
         if (mContext instanceof MyHistoryActivity) {
             if (habitEvent.getCompleted() == Boolean.TRUE) {
-                holder.relativeLayout.setBackgroundColor(0xFF00FF00);
+                holder.constraintLayout.setBackgroundColor(0xFF00FF00);
             } else {
-                holder.relativeLayout.setBackgroundColor(0xFFFF0000);
+                holder.constraintLayout.setBackgroundColor(0xFFFF0000);
             }
         }
         else {
             // default color
-            holder.relativeLayout.setBackgroundColor(0xFFEEEEEE);
+            holder.constraintLayout.setBackgroundColor(0xFFEEEEEE);
         }
     }
 
